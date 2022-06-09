055884d656da7c2fc3e5cfe7cabc79ca36c9edae
glULTRA <7amakurd2002@gmail.com>
2022-06-09 20:37:27 +0300

    Added user data

---
+++ lib/models/user.dart
@@ -0,0 +1,20 @@
+// It's a structure of each user section.
+
+class MegaUser {
+  final String? uid;
+  MegaUser({this.uid});
+}
+
+class MegaUserData {
+  final String? uid;
+  final String? username;
+  final String? fullname;
+  final String? email;
+  
+  MegaUserData({
+    this.uid,
+    this.username,
+    this.fullname,
+    this.email,
+  });
+}
